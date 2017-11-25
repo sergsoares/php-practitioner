75d705df996a89dad8fa58ed2a1ed92293260f06
Sergio Soares <acad.sergio@gmail.com>
2017-11-25 15:27:43 -0300

    Implemented routes and added new views

--- tasks-from-mysql/index.php
+++ tasks-from-mysql/index.php
@@ -3,6 +3,10 @@
 $query = require('core/bootstrap.php');
 $tasks = $query->selectAll('tasks');
 
-require('view/view.php');
+require('core/Router.php');
+$router = new Router;
 
+require('routes.php');
+// var_dump($_SERVER);
+require $router->direct(trim($_SERVER['REQUEST_URI'], '/'));
 ?>
---
+++ tasks-from-mysql/routes.php
@@ -0,0 +1,7 @@
+<?php
+
+
+$router->define([
+    '' => 'controllers/MainController.php',
+    'about' => 'controllers/AboutController.php'
+]);
