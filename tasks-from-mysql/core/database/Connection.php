c6b990a690bf3df46a8dcd0d8cf38b85f44a23b5
Sergio Soares <acad.sergio@gmail.com>
2017-11-25 19:48:44 -0300

    Transform factory in a singleton

--- tasks-from-mysql/core/database/Connection.php
+++ tasks-from-mysql/core/database/Connection.php
@@ -2,18 +2,24 @@
 
 class Connection
 {
+
+    private $connection;
+
     public static function getConnection($config) {
         try{
-            return new PDO(
-                $config['host'].';dbname='.$config['database'],
-                $config['username'],
-                $config['password'],
-                $config['options']
-            );
+            
+            if(! $connection) {
+                return new PDO(
+                    $config['host'].';dbname='.$config['database'],
+                    $config['username'],
+                    $config['password'],
+                    $config['options']
+                );
+            }
         } catch(PDOException $ex) {
             die($ex->getMessage());
         }
 
-        return $conn;
+        return $connection;
     }
 }
