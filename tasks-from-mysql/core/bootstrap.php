91e34bb44fc764ecb2d31ef3f98f7a76357a016a
Sergio Soares <acad.sergio@gmail.com>
2017-11-27 10:40:22 -0300

    Implemented composer in the project

--- tasks-from-mysql/core/bootstrap.php
+++ tasks-from-mysql/core/bootstrap.php
@@ -3,11 +3,6 @@
 $app = [];
 
 $app['config'] = require('config.php');
-require('database/Connection.php');
-require('database/QueryBuilder.php');
-require('Request.php');
-require('core/Router.php');
-
 $app['database'] = new QueryBuilder(
     Connection::getConnection($app['config']['database'])
 );
